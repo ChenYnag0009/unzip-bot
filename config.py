038666b390a5757ae1e4e81e2c63029b35aa72cc
ChenYnag0009 <168895666+ChenYnag0009@users.noreply.github.com>
2025-02-26 23:27:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
         #if os.environ.get("LOGS_CHANNEL").strip("-").isdigit()
         #else os.environ.get("LOGS_CHANNEL", "-1002482663527")
     )
-    MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://cy503127:cy503127@urlupload.tjuxebf.mongodb.net/?retryWrites=true&w=majority&appName=URLUPLOAD")
+    MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://pangphu9:0pSRO3UHIoH5ouAx@cluster0.ipqp2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     MONGODB_DBNAME = os.environ.get("MONGODB_DBNAME", "Unzipper_Bot")
     BOT_OWNER = int(os.environ.get("BOT_OWNER", "1200411908"))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
